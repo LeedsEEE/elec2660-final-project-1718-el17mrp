50702841f4fb884bf3f3ab1002c2e1bc9b6a2b8d
Matthew Pugh [el17mrp] <el17mrp@eentpc87.leeds.ac.uk>
2017-11-20 14:57:15 +0000

    Initial Commit

---
+++ BMITask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMITask
+//
+//  Created by Matthew Pugh [el17mrp] on 20/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMITask/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  BMITask
+//
+//  Created by Matthew Pugh [el17mrp] on 20/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
