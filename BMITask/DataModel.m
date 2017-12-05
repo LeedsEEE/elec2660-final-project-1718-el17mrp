f36e1e0eb33d89240c2ecff8557b7208213e4fe8
Matthew Pugh [el17mrp] <el17mrp@eentpc60.leeds.ac.uk>
2017-12-05 15:58:43 +0000

    All classifications added

--- BMITask/DataModel.m
+++ BMITask/DataModel.m
@@ -18,20 +18,69 @@
         self.bmiclassifications = [NSMutableArray array];
         Classification *VSU =  [[Classification alloc] init];
         VSU.title = @"Very severely underweight";
-        VSU.bmirange = @"15 < BMI < 16";
+        VSU.bmirange = @"BMI < 15";
         VSU.comment = @"You are dangerously underweight and risk health problems";
         VSU.surplus = @"Increase caloric intake by 750 calories each day";
         
         self.bmiclassifications = [NSMutableArray array];
         Classification *SU =  [[Classification alloc] init];
-        SU.title = @"Very severely underweight";
+        SU.title = @"Severely underweight";
         SU.bmirange = @"15 < BMI < 16";
-        SU.comment = @"You are dangerously underweight and risk health problems";
-        SU.surplus = @"Increase caloric intake by 750 calories each day";
+        SU.comment = @"You are very underweight and risk health problems";
+        SU.surplus = @"Increase caloric intake by 500 calories each day";
         
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *U =  [[Classification alloc] init];
+        SU.title = @"Underweight";
+        SU.bmirange = @"16 < BMI < 18.5";
+        SU.comment = @"You are underweight";
+        SU.surplus = @"Increase caloric intake by 250 calories each day";
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *H =  [[Classification alloc] init];
+        SU.title = @"Healthy weight";
+        SU.bmirange = @"18.5 < BMI < 25";
+        SU.comment = @"You are a healthy weight";
+        SU.surplus = @"Maintain your caloric consumption";
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *O =  [[Classification alloc] init];
+        SU.title = @"Overweight";
+        SU.bmirange = @"25 < BMI < 30";
+        SU.comment = @"You are overweight";
+        SU.surplus = @"Decrease your caloric intake by 100 calroies per day";
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *MO =  [[Classification alloc] init];
+        SU.title = @"Moderately Overweight";
+        SU.bmirange = @"30 < BMI < 35";
+        SU.comment = @"You are moderately overweight";
+        SU.surplus = @"Decrease your caloric intake by 250 calroies per day";
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *SO =  [[Classification alloc] init];
+        SU.title = @"Severely Overweight";
+        SU.bmirange = @"35 < BMI < 40";
+        SU.comment = @"You are severely overweight and risk health problems";
+        SU.surplus = @"Decrease your caloric intake by 500 calroies per day";
+        
+        self.bmiclassifications = [NSMutableArray array];
+        Classification *VSO =  [[Classification alloc] init];
+        SU.title = @"Very Severely Overweight";
+        SU.bmirange = @"35 < BMI < 40";
+        SU.comment = @"You are extremely overweight and risk health problems";
+        SU.surplus = @"Decrease your caloric intake by 500 calroies per day and increase activity levels";
         
         
         [self.bmiclassifications addObject:VSU];
+        [self.bmiclassifications addObject:SU];
+        [self.bmiclassifications addObject:U];
+        [self.bmiclassifications addObject:H];
+        [self.bmiclassifications addObject:O];
+        [self.bmiclassifications addObject:MO];
+        [self.bmiclassifications addObject:SO];
+        [self.bmiclassifications addObject:VSO];
         
     }
     return self;
